e162cf4ea3ebf1731f84008d80dadd59df1afa84
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-13 21:45:32 +0600

    Fix bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full  flex flex-col">
-        <NavBar />
+        {/* <NavBar /> */}
         <main className="flex-1"> {children}</main>
         <Footer />
       </body>
