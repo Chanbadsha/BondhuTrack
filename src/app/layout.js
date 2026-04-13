7e5fbbb63cadb031aa2740b440f76234d6228492
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-13 21:46:24 +0600

    Fix bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/shared/Footer";
-import NavBar from "./components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,6 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full  flex flex-col">
-        {/* <NavBar /> */}
         <main className="flex-1"> {children}</main>
         <Footer />
       </body>
